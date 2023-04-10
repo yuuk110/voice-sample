514e90035805002f84522e85cddad7acd54c86b3
yuuk110 <yuuki_suzuki@tglab.jp>
2023-04-10 20:50:49 +0900

    プッシュ

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -76,6 +76,7 @@
             </div>
 
             <div class="mt-3 space-y-1">
+                
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
